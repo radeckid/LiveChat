fef4d03b1ab4ac2896af22ec847e30d57982e1ad
radeckid <damianradecki97@gmail.com>
2020-03-17 11:58:45 +0100

    First Commit

---
+++ LiveChatRegisterLogin/Controllers/PersonsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiveChatRegisterLogin.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    public class PersonsController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Person>> Get()
+        {
+            return new Person[] {
+            new Person(1,"Damian", "Radecki"),
+            new Person(2, "Damian", "Kurkiewicz")
+            };
+        }
+
+    }
+}
---
+++ LiveChatRegisterLogin/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiveChatRegisterLogin
+{
+    public class Person
+    {
+        public int id {get; set;}
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public Person(int id, string name, string surname)
+        {
+            this.id = id;
+            Name = name;
+            Surname = surname;
+        }
+    }
+}
